b771a41e2c6ec06655af3b007347b6080d1736a9
extrodnare274 <76926053+extrodnare274@users.noreply.github.com>
2023-10-22 19:04:12 -0400

    Update take_screenshot.py

--- take_screenshot.py
+++ take_screenshot.py
@@ -48,7 +48,17 @@ while True:
                 
                 # WRITE THE CODE HERE   
 
+                image = pyatogui.screenshot()
+                image = cv2.cvtColor(np.array(image), cv2.COLOR_RGB2BGR)
+                cv2.imwrite("in_memory_to_disk.png", image)
 
+
+                pyatogui.screenshot("straight_to_disk.png")
+
+
+                image = cv2.imread("straight_to_disk.png")
+                cv2.imshow("Screenshot", imutils.resize(image, width=600))
+            
 
 
 
